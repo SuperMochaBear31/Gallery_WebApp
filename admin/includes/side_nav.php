c71da586abf96fd4d760fa8bc19195097fdcc701
Ian Chin <skyhawk3102000@yahoo.com>
2018-03-14 09:11:51 -0400

    Add files via upload

---
+++ admin/includes/side_nav.php
@@ -0,0 +1,20 @@
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav side-nav">
+                    <li>
+                        <a href="index.php"><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="users.php"><i class="fas fa-users"></i> Users</a>
+                    </li>
+                    <li>
+                        <a href="upload.php"><i class="fas fa-upload"></i> Upload</a>
+                    </li>
+					 <li>
+                        <a href="photos.php"><i class="fas fa-camera-retro"></i> Photos</a>
+                    </li>
+                    <li>
+                        <a href="comments.php"><i class="far fa-comments"></i> Comments</a>
+                    </li>
+
+                </ul>
+            </div>
